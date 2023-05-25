4b2120d40094fe2b99abb460e7f6ab320eea681a
Saran-386 <130638751+Saran-386@users.noreply.github.com>
2023-05-25 05:58:14 +0000

    2nd

--- Looping/1.py
+++ Looping/1.py
@@ -1,6 +1,8 @@
 e=0
 o=0
-for i in range(20,51):
+lower=int(input("Enter lower range: "))
+upper=int(input("Enter upper range: "))
+for i in range(lower,upper+1):
     if (i%2==0):
         e=e+1
     else:
