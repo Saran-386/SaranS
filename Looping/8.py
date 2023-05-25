651a827779bd93d03a8295842124a41f9827589f
Saran-386 <130638751+Saran-386@users.noreply.github.com>
2023-05-25 05:40:30 +0000

    2nd

--- Looping/8.py
+++ Looping/8.py
@@ -2,10 +2,11 @@ n=int(input("Enter lower range: "))
 n2=int(input("Enter upper range: "))
 for i in range(n,n2+1):
     order = len(str(i))
-    sum = 0
+    s = 0
     temp = i
     while temp > 0:
         digit = temp % 10
-        sum += digit ** order
+        s += digit ** order
         temp //= 10
-    print(sum)
+    print(s)
+    break
