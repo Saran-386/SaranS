3fba320891730807bb533fd44811c79a265e6564
Saran-386 <130638751+Saran-386@users.noreply.github.com>
2023-06-08 05:40:51 +0000

    2nd

--- Functional/4.py
+++ Functional/4.py
@@ -5,10 +5,12 @@ def recur_fibo(n):
        return(recur_fibo(n-1) + recur_fibo(n-2))
 
 nterms = int(input("Enter number: "))
-
+s=0
 if nterms <= 0:
    print("Plese enter a positive integer")
 else:
    print("Fibonacci sequence:")
    for i in range(nterms):
-       print(recur_fibo(i))
+        s=s+i
+
+print(s)
